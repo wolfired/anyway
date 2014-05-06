d166a5a4e0f4b5b57e9b61fca313295fab5d82de
LinkWu <wjianl@qq.com>
2014-05-06 16:01:59 +0800

    test travis-ci with bash

--- script.sh
+++ script.sh
@@ -25,5 +25,5 @@ else
 	curl "http://download.macromedia.com/get/flashplayer/updaters/$PLAYER_VERSION_MAJOR/playerglobal$PLAYER_VERSION_MAJOR_$PLAYER_VERSION_MINOR.swc" > $PLAYERGLOBAL_HOME/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/playerglobal.swc
 fi
 
-ant compile_core -Dconfig.player.version=$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR -Dconfig.swf.version=24 -DFLEX_HOME=./$APACHE_FLEX
+ant compile_core -Dconfig.player.version=$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR -Dconfig.swf.version=24 -DFLEX_HOME=$APACHE_FLEX_HOME
 
