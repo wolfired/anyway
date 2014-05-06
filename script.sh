0b4c998321278e028bc4a5a624afbcd5780fd198
LinkWu <wjianl@qq.com>
2014-05-06 15:10:23 +0800

    test travis-ci with bash

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 APACHE_FLEX=apache-flex-sdk-4.12.1-bin
 if [ ! -d "$APACHE_FLEX" ]; then
-	curl "http://mirrors.cnnic.cn/apache/flex/4.12.1/binaries/$APACHE_FLEX.tar.gz" >> $APACHE_FLEX.tar.gz
+	echo "download"
+	curl "http://mirrors.cnnic.cn/apache/flex/4.12.1/binaries/$APACHE_FLEX.tar.gz" > $APACHE_FLEX.tar.gz
 	tar -zxf $APACHE_FLEX.tar.gz
 fi
 
 PLAYERGLOBAL_HOME=$APACHE_FLEX/frameworks/libs/player
 PLAYER_VERSION_MAJOR=13
 PLAYER_VERSION_MINOR=0
-if [ ! -d "$APACHE_FLEX/frameworks/libs/player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/" ]; then
-	mkdir -p player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR
-	curl "http://download.macromedia.com/get/flashplayer/updaters/$PLAYER_VERSION_MAJOR/playerglobal$PLAYER_VERSION_MAJOR_$PLAYER_VERSION_MINOR.swc" >> $APACHE_FLEX/frameworks/libs/player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/playerglobal.swc
+if [ ! -d "$PLAYERGLOBAL_HOME/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/" ]; then
+	echo "download"
+	mkdir $PLAYERGLOBAL_HOME/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/
+	curl "http://download.macromedia.com/get/flashplayer/updaters/$PLAYER_VERSION_MAJOR/playerglobal$PLAYER_VERSION_MAJOR_$PLAYER_VERSION_MINOR.swc" > $PLAYERGLOBAL_HOME/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/playerglobal.swc
 fi
 
 ant compile_core -Dconfig.player.version=$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR -Dconfig.swf.version=24 -DFLEX_HOME=./$APACHE_FLEX
