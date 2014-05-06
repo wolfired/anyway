903fcb2470e6d3a714cf2f8fae7958d10a635635
LinkWu <wjianl@qq.com>
2014-05-06 13:26:30 +0800

    test travis-ci

---
+++ setup.sh
@@ -0,0 +1,17 @@
+sudo apt-get update -qq
+sudo apt-get install -qq ant
+
+set APACHE_FLEX=apache-flex-sdk-4.12.1-bin
+if [[ ! -d "$APACHE_FLEX" ]]; then
+	curl 'http://mirrors.cnnic.cn/apache/flex/4.12.1/binaries/$APACHE_FLEX.tar.gz'
+	tar -zxf $APACHE_FLEX.tar.gz
+fi
+
+set PLAYER_VERSION_MAJOR=13
+set PLAYER_VERSION_MINOR=0
+if [[ ! -d "$APACHE_FLEX/frameworks/libs/player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/" ]]; then
+	mkdir -p player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR
+	curl 'http://download.macromedia.com/get/flashplayer/updaters/$PLAYER_VERSION_MAJOR/playerglobal$PLAYER_VERSION_MAJOR_$PLAYER_VERSION_MINOR.swc' >> $APACHE_FLEX/frameworks/libs/player/$PLAYER_VERSION_MAJOR.$PLAYER_VERSION_MINOR/playerglobal.swc
+fi
+
+
